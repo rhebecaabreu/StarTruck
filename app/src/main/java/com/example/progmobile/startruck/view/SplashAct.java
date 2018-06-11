862a5d994d1454676705ea32d93d7a8125d2c3f5
Augusto Joffer <gutojoffer@gmail.com>
2018-06-11 15:43:10 -0400

    update

--- app/src/main/java/com/example/progmobile/startruck/view/SplashAct.java
+++ app/src/main/java/com/example/progmobile/startruck/view/SplashAct.java
@@ -5,7 +5,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.progmobile.startruck.R;
-
 public class SplashAct extends AppCompatActivity {
 
     @Override
